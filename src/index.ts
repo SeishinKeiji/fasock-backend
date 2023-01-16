252fc4c242168eb757c701efecffc728eee1cf4f
SeishinKeiji <ahmadxgani01@gmail.com>
2023-01-16 13:09:31 +0700

    chore: turn on the logger

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import handler from "./handler";
 import socketIO from "plugins/socket.io";
 import { datasource } from "database";
 
-const server = fastify();
+const server = fastify({ logger: true });
 server.register(socketIO, {
   cors: {
     origin: "*",
@@ -16,12 +16,11 @@ server.register(socketIO, {
 server.ready(async (error) => {
   if (error) console.log(error), process.exit(1);
   await datasource.initialize();
-  console.log(datasource.isInitialized ? "connection was successfuly created" : "failed to connect database");
+  server.log.info(datasource.isInitialized ? "connection was successfuly created" : "failed to connect database");
   const onConnection = (socket: Socket) => {
     handler(server.io, socket);
   };
   server.io.on("connection", onConnection);
-  console.log(`Server listening at ${process.env.HOST_NAME}:${process.env.HOST_PORT}`);
 });
 
 server.listen({ port: (process.env.HOST_PORT as any) || 4040, host: process.env.HOST_NAME || "localhost" });
